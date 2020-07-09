b6c5051d0e24e5e5f07f6a4d1b7784e441a8ac3a
abnerpsouza99 <abnertecnicosenac@gmail.com>
2020-07-09 01:26:50 -0300

    Inserts the Dark Mode with JavaScript

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 const target = document.querySelectorAll('[data-anime]');
 const animationClass = 'animate';
 
-function animeScroll(){
+function animeScroll() {
   const windowTop = window.pageYOffset;
-  target.forEach(function(element) {
-    if(windowTop > element.OffsetTop){
+  target.forEach(function (element) {
+    if (windowTop > element.OffsetTop) {
       element.classList.add(animationClass);
     } else {
       element.classList.remove(animationClass);
@@ -12,6 +12,33 @@ function animeScroll(){
   })
 }
 
-window.addEventListener("scroll", function(){
+window.addEventListener("scroll", function () {
   animeScroll();
 })
+
+
+// Checkbox
+
+//const toggle = document.querySelector('#toggleButton')
+
+// function darkMode() {
+//   if (toggle.checked) {
+//     // main.style.toggle(toggle.classList.add('.dark'))
+//     main.classList.add('dark')
+//   }
+//   else if (!toggle.checked) {
+//     main.classList.remove('dark')
+//   }
+// }
+
+
+
+const main = document.querySelector('main')
+const darkModeToggle = document.querySelector('.darkMode')
+const darkModeCircle = document.querySelector('.dark-mode-circle')
+
+function darkMode() {
+  main.classList.toggle('dark')
+  darkModeCircle.classList.toggle('circle-right')
+  darkModeToggle.classList.toggle('dark-mode-on')
+}
